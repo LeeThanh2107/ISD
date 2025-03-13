49519c37074761e481e34282eb6e05a7508db575
Lê Tất Thành <leethanh210703@gmail.com>
2025-03-13 23:58:54 +0700

    Create user

--- src/main/java/com/example/CMS/SecurityConfig.java
+++ src/main/java/com/example/CMS/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow frontend origin (local)
-//        configuration.setAllowedOrigins(List.of("http://cms-lyluanchinhtri.up.railway.app")); // Allow frontend origin (develop)
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow frontend origin (local)
+        configuration.setAllowedOrigins(List.of("http://cms-lyluanchinhtri.up.railway.app")); // Allow frontend origin (develop)
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
