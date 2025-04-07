afc0fcb7c941306dffa23e35124df763121487e4
LeeThanh2107 <leesuo19@gmail.com>
2025-04-07 23:57:26 +0700

    Init change password

--- src/main/java/com/example/CMS/Controller/AuthController.java
+++ src/main/java/com/example/CMS/Controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.CMS.Controller;
 
+import com.example.CMS.Common.ResponseUtils;
 import com.example.CMS.Model.User;
 import com.example.CMS.Services.JwtService;
 import com.example.CMS.Services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +16,6 @@ import java.util.Map;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/login")
 public class AuthController {
 
     private final UserService userService;
@@ -25,10 +26,9 @@ public class AuthController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         // Lấy user từ database
-        System.out.println(loginRequest);
         Optional<User> user = userService.findByUsername(loginRequest.getUsername());
         if (user.isEmpty() || !passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Sai tài khoản hoặc mật khẩu!");
@@ -43,7 +43,20 @@ public class AuthController {
         response.put("name", user.get().getName());
         return ResponseEntity.ok(response);
     }
-
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody LoginRequest loginRequest) {
+        Optional<User> user = userService.findByUsername(loginRequest.getUsername());
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Không tìm thấy tài khoản!");
+        }
+        try {
+            user.get().setPassword(this.passwordEncoder.encode(loginRequest.getPassword()));
+            userService.resetPassword(user.get());
+            return ResponseUtils.success("Change password successfully");
+        }catch(Exception e){
+            return ResponseUtils.error(HttpStatus.INTERNAL_SERVER_ERROR,"Something bad happened!");
+        }
+    }
     public static class LoginRequest {
         private String username;
         private String password;
